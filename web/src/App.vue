a5fdfaa772044250a3f2f0cee5545216b5a4f0b5
Jeff Waelchi <20675545+chenlike@users.noreply.github.com>
2024-05-26 22:17:54 +0800

    change production url

--- web/src/App.vue
+++ web/src/App.vue
@@ -38,7 +38,7 @@ async function getTAO() {
 
 
   loading.value = true
-  const res = await axios.post("https://tea-transfer-server-dkvcaktgb-chenlikes-projects.vercel.app/api/sendTAO", {
+  const res = await axios.post("https://tea-transfer-server.vercel.app/api/sendTAO", {
     address: address.value
   })
   result.value = res.data
